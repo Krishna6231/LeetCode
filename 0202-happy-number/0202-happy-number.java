421d080350e711cfb14aebd3601df06e82a4a74f
Krishna <102744472+Krishna6231@users.noreply.github.com>
2024-08-05 16:53:30 +0530

    Update 0202-happy-number.java

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,3 +1,6 @@
+//HashSet doesnt Store Duplicates. When calculating happy number , pattern can repeat like 1,2,3,1,2,3 So, if pattern is there it is not a happy number 
+// Here HashSet doesnt store duplicates it stores the first added ele only , even if we try to add another duplicate . So if there is a pattern it will be in HashSet 
+//Once it is detected it is repeating number loop is exited as it will be false and it will not be equal to 1 and if no pattern is there we will get answer as true
 import java.util.*;
 class Solution {
     public boolean isHappy(int n) {
