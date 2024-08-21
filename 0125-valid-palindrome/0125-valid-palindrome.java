c710ab97ac15ba51210e60fb2f1b4080967ce091
Krishna <102744472+Krishna6231@users.noreply.github.com>
2024-08-21 22:52:23 +0530

    Time: 2 ms (99.11%), Space: 42.8 MB (75.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,29 +2,42 @@ class Solution {
     public boolean isPalindrome(String s) {
         if (s == null || s.trim().isEmpty())
             return true;
-        s = s.replace(" ", "").toLowerCase();
+        // s = s.replace(" ", "").toLowerCase();
 
-        StringBuilder sb = new StringBuilder();
+        // StringBuilder sb = new StringBuilder();
 
-        for(int i = 0 ;i<s.length();i++){
-            char c = s.charAt(i);
-            if(Character.isDigit(c) || Character.isAlphabetic(c)){
-                sb.append(c);
-            }
+        // for(int i = 0 ;i<s.length();i++){
+        //     char c = s.charAt(i);
+        //     if(Character.isDigit(c) || Character.isAlphabetic(c)){
+        //         sb.append(c);
+        //     }
             
-        }
+        // }
         int i = 0;
-        int j = sb.length() - 1;
+        int j = s.length() - 1;
 
-        while (i < j) {
-            if (sb.charAt(i) == sb.charAt(j)) {
+        // while (i < j) {
+        //     if (sb.charAt(i) == sb.charAt(j)) {
+        //         i++;
+        //         j--;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
                 j--;
-            } else {
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
                 return false;
             }
+            i++;
+            j--;
         }
-
         return true;
     }
 }
