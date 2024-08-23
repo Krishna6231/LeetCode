05098df1ef4c7d559def530a18c32f2e8de8f7ab
Krishna <102744472+Krishna6231@users.noreply.github.com>
2024-08-23 22:32:00 +0530

    Time: 313 ms (9.22%), Space: 47.1 MB (38.1%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,32 +1,35 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
-        // for(int i =0 ;i < numbers.length -1 ;i++){
-        //     for(int j=i+1 ; j < numbers.length ;j++){
-        //         if(numbers[i]+numbers[j]==target){
-        //             result[0]=i+1;
-        //             result[1]=j+1;
-        //             break;
-        //         }
-        //     }
-        // }
-        //                     return result;
-
-        int i=0;int j= numbers.length-1;
-        while(i<j){
-            if(numbers[i]+numbers[j]==target){
+        for(int i =0 ;i < numbers.length -1 ;i++){
+            for(int j=i+1 ; j < numbers.length ;j++){
+                if(numbers[i]+numbers[j]==target){
                     result[0]=i+1;
                     result[1]=j+1;
                     break;
                 }
-                else if(numbers[i]+numbers[j]>target){
-                    j--;
-                }
-                else{
-                    i++;
-                }
+                    else if(numbers[i]+numbers[j]>target) {
+                        break;
+                    }
+            }
         }
-        return result;
+                            return result;
+
+        // int i=0;int j= numbers.length-1;
+        // while(i<j){
+        //     if(numbers[i]+numbers[j]==target){
+        //             result[0]=i+1;
+        //             result[1]=j+1;
+        //             break;
+        //         }
+        //         else if(numbers[i]+numbers[j]>target){
+        //             j--;
+        //         }
+        //         else{
+        //             i++;
+        //         }
+        // }
+        // return result;
 
     }
 }
