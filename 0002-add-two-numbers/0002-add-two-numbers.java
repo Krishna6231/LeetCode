42f006e4dd7908a11dc30b153900843067d2936e
Krishna <102744472+Krishna6231@users.noreply.github.com>
2025-01-06 08:14:27 +0530

    Time: 1 ms (100%), Space: 44.7 MB (25.85%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,27 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
+    public ListNode addTwoNumbers(ListNode A, ListNode B) {
+        ListNode head1 = A;
+        ListNode head2 = B;
+        ListNode dummy=new ListNode(-10);
+        ListNode td = dummy;
+        int carry=0;
+        while(head1!=null && head2!=null){
+            
+            int add = head1.val+head2.val+carry;
+            carry=0;
+            if(add>9){
+                carry = add/10;
+                ListNode num = new ListNode(add%10);
+                dummy.next = num;
+                dummy=num;
+            }
+            else{
+                ListNode num = new ListNode(add);
+                dummy.next = num;
+                dummy=num;
+            }
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+            head1=head1.next;
+            head2=head2.next;
         }
+        while(head1!=null){
+            int add = head1.val+carry;
+            carry=0;
+            if(add>9){
+                carry = add/10;
+                ListNode num = new ListNode(add%10);
+                dummy.next = num;
+                dummy=num;
+                }
+            else{
+                ListNode num = new ListNode(add);
+                dummy.next = num;
+                dummy=num;
+            }
+            head1=head1.next;
+            
+        }
+        while(head2!=null){
+            int add = head2.val+carry;
 
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
+            carry=0;
+            if(add>9){
+                carry = add/10;
+                ListNode num = new ListNode(add%10);
+                dummy.next = num;
+                dummy=num;
+            }
+            else{
+                ListNode num = new ListNode(add);
+                dummy.next = num;
+                dummy=num;
+            }
+            head2=head2.next;
+        }
+        
+        
+        if(carry>0){
+        ListNode num = new ListNode(carry);
+                dummy.next = num;
+                dummy=num;
+        }
+        return (td.next);
     }
 }
